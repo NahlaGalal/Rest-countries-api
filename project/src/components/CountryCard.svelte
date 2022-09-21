44a869894303cfa2e5d6a9090fc55c4d3dd3f6cd
NahlaGalal <nahlaglal@gmail.com>
2022-09-21 23:36:13 +0200

    Add Country cards

---
+++ project/src/components/CountryCard.svelte
@@ -0,0 +1,50 @@
+<script type="ts">
+  export let country: {
+    img: string;
+    name: string;
+    population: number;
+    region: string;
+    capital: string;
+  } = {
+    img: "",
+    name: "",
+    population: 0,
+    region: "",
+    capital: ""
+  };
+</script>
+
+<section>
+  <img src={country.img} alt="" />
+  <p class="name">{country.name}</p>
+  <p><span>Population: </span> {country.population}</p>
+  <p><span>Region: </span> {country.region}</p>
+  <p><span>Capital: </span> {country.capital}</p>
+</section>
+
+<style>
+  section {
+    background-color: var(--light-elements);
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+    color: var(--light-text);
+    padding-bottom: 40px;
+  }
+
+  img {
+    width: 100%;
+    height: 150px;
+  }
+
+  .name {
+    font-weight: 800;
+    padding: 16px;
+  }
+
+  p {
+    padding-left: 16px;
+  }
+
+  p span {
+    font-weight: 600;
+  }
+</style>
--- project/src/pages/Home.svelte
+++ project/src/pages/Home.svelte
@@ -1,17 +1,69 @@
 <script type="ts">
-  import HomeFilters from '../components/HomeFilters.svelte';
+  import { onMount } from "svelte";
+  import CountryCard from "../components/CountryCard.svelte";
+  import HomeFilters from "../components/HomeFilters.svelte";
+
+  let countries: {
+    img: string;
+    name: string;
+    population: number;
+    region: string;
+    capital: string;
+  }[] = [];
+
+  onMount(async () => {
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const data = await res.json();
+
+    countries = data.map((country: any) => ({
+      img: country.flags.png,
+      name: country.name.common,
+      population: country.population,
+      region: country.region,
+      capital: country.capital?.join(", "),
+    }));
+  });
 </script>
 
 <div class="container">
   <HomeFilters />
 
-  <main>
+  <main class="cards">
     <!-- Cards -->
+    {#each countries as country}
+      <CountryCard {country} />
+    {:else}
+      <p>Loading...</p>
+    {/each}
   </main>
 </div>
 
 <style>
   .container {
     padding: 40px 10%;
+  }
+
+  .cards {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 24px;
+  }
+
+  @media screen and (max-width: 1200px) {
+    .cards {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    .cards {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  @media screen and (max-width: 520px) {
+    .cards {
+      grid-template-columns: 1fr;
+    }
   }
 </style>
