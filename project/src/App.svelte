6773232fca16db10699f0efd164f5787f77bdb3b
NahlaGalal <nahlaglal@gmail.com>
2022-09-21 17:28:19 +0200

    Add navbar

--- project/src/App.svelte
+++ project/src/App.svelte
@@ -1,19 +1,32 @@
 <script lang="ts">
+  import Navbar from "./Navbar.svelte";
 </script>
 
-<main>
-</main>
+<Navbar />
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap');
+  @import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap");
+	
+  :global(:root) {
+    --dark-text: hsl(0, 0%, 100%);
+    --dark-elements: hsl(209, 23%, 22%);
+    --dark-background: hsl(207, 26%, 17%);
+    --light-text: hsl(200, 15%, 8%);
+    --light-elements: hsl(0, 0%, 100%);
+    --light-background: hsl(0, 0%, 98%);
+    --light-input: hsl(0, 0%, 52%);
+  }
 
-	:global(:root) {
-		--dark-text: hsl(0, 0%, 100%);
-		--dark-elements: hsl(209, 23%, 22%);
-		--dark-background: hsl(207, 26%, 17%);
-		--light-text: hsl(200, 15%, 8%);
-		--light-elements: hsl(0, 0%, 100%);
-		--light-background: hsl(0, 0%, 98%);
-		--light-input: hsl(0, 0%, 52%);
-	}
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    font-family: "Nunito Sans";
+		background-color: var(--light-background);
+  }
+
+  :global(*, button, input, select, textarea) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 </style>
---
+++ project/src/Navbar.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import FaRegMoon from 'svelte-icons/fa/FaRegMoon.svelte'
+</script>
+
+<nav>
+  <h1>Where in the world?</h1>
+
+  <button>
+    <span>
+      <FaRegMoon />
+    </span>
+    Dark Mode
+  </button>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 10%;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+    color: var(--light-text);
+  }
+  
+  button {
+    display: flex;
+    background-color: transparent;
+    border: 0;
+    cursor: pointer;
+    gap: 8px;
+    color: var(--light-text);
+    align-items: center;
+  }
+
+  button:focus {
+    background-color: transparent;
+  }
+
+  span {
+    display: block;
+    width: 16px;
+    height: 16px;
+  }
+</style>
