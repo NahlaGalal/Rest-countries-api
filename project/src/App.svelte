0ceb367c8c3ec43d4a939d08a8ccd8d642468266
NahlaGalal <nahlaglal@gmail.com>
2022-09-21 17:05:37 +0200

    Add setup data

---
+++ project/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<main>
+</main>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap');
+
+	:global(:root) {
+		--dark-text: hsl(0, 0%, 100%);
+		--dark-elements: hsl(209, 23%, 22%);
+		--dark-background: hsl(207, 26%, 17%);
+		--light-text: hsl(200, 15%, 8%);
+		--light-elements: hsl(0, 0%, 100%);
+		--light-background: hsl(0, 0%, 98%);
+		--light-input: hsl(0, 0%, 52%);
+	}
+</style>
